c7ae28a755f53dc8c8a02a6d960496a8eaa79192
sentidosfamily <134061501+sentidosfamily@users.noreply.github.com>
2025-11-14 18:40:57 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,23 @@ import { ReactNode } from 'react';
 import ClientProvider from './context/clientProvider'; // un wrapper que sea "use client"
 
 export const metadata = {
-  title: 'Sentidos',
+  title: 'Sentidos Padres',
   description: 'Comunidad centrada en compartir experiencias',
+  icons: {
+    icon: 'favicon.ico',          // favicon
+    apple: 'favicon.ico',          // iOS icon
+  },
+  openGraph: {
+    title: 'Sentidos Padres',
+    description: 'Comunidad centrada en compartir experiencias',
+    images: ['/assets/sentidoslogo.png'],      // preview en redes sociales
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Sentidos Padres',
+    description: 'Comunidad centrada en compartir experiencias',
+    images: ['/assets/sentidoslogo.png'],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
