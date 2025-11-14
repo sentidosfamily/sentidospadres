10e51104b89c0882e3f6f3d7f03dfbe9b35e5185
sentidosfamily <134061501+sentidosfamily@users.noreply.github.com>
2025-11-14 18:22:35 -0300

    Update page.tsx

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+// app/register/page.tsx
+import React from 'react';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div>
+      Registro
+    </div>
+  );
+};
+
+export default RegisterPage; // <- esto es obligatorio
